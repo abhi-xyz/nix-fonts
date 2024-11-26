5ca4ff02f70b760a3b66dec5c80ac1c6dbcc1db3
Abhinandh S <ugabhi@proton.me>
2024-11-26 14:43:53 +0530

    Refactoring

--- module.nix
+++ module.nix
@@ -1,33 +1,29 @@
 { config, pkgs, lib, ... }: 
 let
-  # Define available font packages
   availablefonts = {
     lora = pkgs.callPackage ./fonts/lora/default.nix { stdenvnocc = pkgs.stdenv; };
     inter = pkgs.callPackage ./fonts/inter/default.nix { stdenvnocc = pkgs.stdenv; };
     eb-garamond = pkgs.callPackage ./fonts/eb-garamond/default.nix { stdenvnocc = pkgs.stdenv; };
   };
 
-  # Define the fonts option for selecting a list of font names
   fonts = lib.mkOption {
     type = lib.types.listOf lib.types.str;
-    default = ["lora"];  # Default to "lora"
+    default = ["lora"];
     description = "List of fonts to enable. Available options: lora, inter.";
   };
 
-  # Map selected font names to font packages directly
   selectedfontPackages = lib.listToAttrs (map (fontName: {
     name = fontName;
-    value = availablefonts.${fontName};  # Directly use the font package
+    value = availablefonts.${fontName}; 
   }) config.fonts.nix-fonts.fonts);
 
 in {
   options.fonts.nix-fonts = {
     enable = lib.mkEnableOption "Enable the nix-fonts.";
-    fonts = fonts;  # Include the fonts option in the module
+    fonts = fonts;  
   };
 
   config = lib.mkIf config.fonts.nix-fonts.enable {
-    # Add selected font packages to the fonts list
     fonts.packages = lib.attrValues selectedfontPackages;
   };
 }
